e5ca9e3c9fec82bc747acefdc2f2128d1382c09a
ThomasAriza1227 <arizathomas2007@gmail.com>
2026-08-24 10:41:41 -0500

    Se quito la constante de productos de app.jsx y se agrego en el archivo de productos.js

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,6 @@ const aplicarIva = (precio) => {
   return precio * 1.19;
 };
 
-const productos = [
-  { id: 1, nombre: 'Mouse', precio: 50000, stock: 5 },
-  { id: 2, nombre: 'Teclado', precio: 90000, stock: 0 },
-  { id: 3, nombre: 'Monitor', precio: 650000, stock: 3 },
-  { id: 4, nombre: 'Mousepad', precio: 35000, stock: 6 },
-  { id: 5, nombre: 'Mando Xbox', precio: 90000, stock: 3 },
-  { id: 6, nombre: 'Mando PS5', precio: 90000, stock: 2 },
-  { id: 7, nombre: 'Xbox X', precio: 240000, stock: 6 },
-  { id: 8, nombre: 'PLay 5', precio:250000, stock: 4 },
-  { id: 9, nombre: 'Torre Gamer', precio: 1000000, stock: 3 },
-  { id: 10, nombre: 'Celular', precio: 150000, stock: 1 }
-];
-
 
 const nombres = productos.map(producto => {
   return producto.nombre;
--- src/data/producto.js
+++
@@ -1,4 +0,0 @@
-export const productos = [
-  { id: 1, nombre: 'Mouse', precio: 50000, stock: 5 },
-  { id: 2, nombre: 'Teclado', precio: 90000, stock: 0 }
-];
